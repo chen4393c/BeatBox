3ebb4da24b40d8641684468e9be6c4754dc452fc
chen4393c <chen4393c@gmail.com>
2018-08-09 03:44:10 -0500

    Creating BeatBox

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}

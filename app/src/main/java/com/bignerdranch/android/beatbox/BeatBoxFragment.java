63c5c6a1d3033f14f7884bbbe357f6711ca8bc93
chen4393c <chen4393c@gmail.com>
2018-08-09 15:50:55 -0500

    Simple data binding

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -1,6 +1,18 @@
 package com.bignerdranch.android.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bignerdranch.android.beatbox.databinding.FragmentBeatBoxBinding;
+import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -8,5 +20,48 @@ import android.support.v4.app.Fragment;
 public class BeatBoxFragment extends Fragment {
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        binding.recyclerView.setAdapter(new SoundAdapter());
+
+        return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        private SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        @NonNull
+        @Override
+        public SoundHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil
+                    .inflate(inflater, R.layout.list_item_sound, parent, false);
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SoundHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
